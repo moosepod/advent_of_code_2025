e43c4daf0d2ca96ad4f955b837751125b0297a94
Matt Christensen <mchristensen@moosepod.com>
2025-12-01 07:56:46 -0500

    Day 1 silver

--- day_1/day_1.py
+++ day_1/day_1.py
@@ -1,5 +1,18 @@
-def main():
-    print("Test")
+def main(path="day_1/inputs/silver.txt"):
+    dial = 50
+    count = 0
+    with open(path) as f:
+        for line in f.read().split("\n"):
+            if line:
+                direction = 1 if line[0] == 'R' else -1
+                amount = int(line[1:])
+                dial = (dial + (amount * direction)) % 100
+                if dial == 0:
+                    count += 1
+
+    print(f"Final dial: {dial}")
+    print(f"Answer: {count}")
+    
     
 
 if __name__ == "__main__":
