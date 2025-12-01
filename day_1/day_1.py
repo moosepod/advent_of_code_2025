26377f8fdff6da6fbfd73becd31246f09a46f7cb
Matt Christensen <mchristensen@moosepod.com>
2025-12-01 09:06:56 -0500

    Cleaned up day 1

--- day_1/day_1.py
+++ day_1/day_1.py
@@ -1,47 +1,46 @@
 import argparse
 
-def solve_silver(path="day_1/inputs/input.txt"):
-    dial = 50
-    count = 0
-    with open(path) as f:
-        for line in f.read().split("\n"):
-            if line:
-                direction = 1 if line[0] == 'R' else -1
-                amount = int(line[1:])
-                dial = (dial + (amount * direction)) % 100
-                if dial == 0:
-                    count += 1
+def iterate_silver(dial, count, direction, amount):
+    dial = (dial + (amount * direction)) % 100
+    if dial == 0:
+        count += 1
 
-    print(f"Final dial: {dial}")
-    print(f"Answer: {count}")
+    return dial, count
 
-def solve_gold(path="day_1/inputs/input.txt"):
-    dial = 50
-    count = 0
-    with open(path) as f:
-        for line in f.read().split("\n"):
-            if line:
-                initial_dial = dial
-                direction = 1 if line[0] == 'R' else -1
-                amount = int(line[1:])
-                dial += amount * direction
-                if dial <= 0 and initial_dial != 0:
-                    count += 1
-                count += int(abs(dial)/100)
-                dial = dial % 100
-
+def iterate_gold(dial, count, direction, amount):
+    initial_dial = dial
+    dial += amount * direction
+    if dial <= 0 and initial_dial != 0:
+        count += 1
+    count += int(abs(dial)/100)
+    dial = dial % 100
+    return dial, count
                 
     print(f"Final dial: {dial}")                    
-    print(f"Answer: {count}")    
+    print(f"Answer: {count}")
+
+
+def solve(iterate_f, path="day_1/inputs/input.txt", initial_dial=50):
+    dial = 50
+    count = 0
+    with open(path) as f:
+        for line in f.read().split("\n"):
+            if line:
+                amount = int(line[1:])            
+                direction = 1 if line[0] == 'R' else -1
+                dial, count = iterate_f(dial, count, direction, amount)
+
+    print(f"Final dial: {dial}")                    
+    print(f"Answer: {count}")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("star")
     args = parser.parse_args()
     if args.star == "silver":
-        solve_silver()
+        solve(iterate_silver)
     elif args.star == "gold":
-        solve_gold()
+        solve(iterate_gold)
     else:
         print("First argument must be silver or gold")
 
