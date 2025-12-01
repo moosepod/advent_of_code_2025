8a60d38755dcea8fa8c89a48bac08fd07c58d803
Matt Christensen <mchristensen@moosepod.com>
2025-12-01 08:46:07 -0500

    Naive solution to gold star

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
 .PHONY: day-1-silver
 
 day-1-silver: day_1/day_1.py
-	uv run day_1/day_1.py --silver
+	uv run day_1/day_1.py silver
+
+day-1-gold: day_1/day_1.py
+	uv run day_1/day_1.py gold
 
--- day_1/day_1.py
+++ day_1/day_1.py
@@ -1,4 +1,6 @@
-def main(path="day_1/inputs/silver.txt"):
+import argparse
+
+def solve_silver(path="day_1/inputs/input.txt"):
     dial = 50
     count = 0
     with open(path) as f:
@@ -12,8 +14,36 @@ def main(path="day_1/inputs/silver.txt"):
 
     print(f"Final dial: {dial}")
     print(f"Answer: {count}")
-    
-    
+
+def solve_gold(path="day_1/inputs/input.txt"):
+    dial = 50
+    count = 0
+    with open(path) as f:
+        for line in f.read().split("\n"):
+            if line:
+                direction = 1 if line[0] == 'R' else -1
+                amount = int(line[1:])                                    
+                for i in range(0, amount):
+                    dial += direction
+                    if dial == 100:
+                        count += 1
+                        dial = 0
+                    elif dial == 0:
+                        count += 1
+                    elif dial < 0:
+                        dial = 99
+                
+    print(f"Final dial: {dial}")                    
+    print(f"Answer: {count}")    
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("star")
+    args = parser.parse_args()
+    if args.star == "silver":
+        solve_silver()
+    elif args.star == "gold":
+        solve_gold()
+    else:
+        print("First argument must be silver or gold")
+
