8c8cb30aefacb1d77046c4e8b759d3c1a2a3c48e
Matthew Christensen <mchristensen@moosepod.com>
2025-12-03 06:37:53 -0500

    Day 3 silver naive

--- day_3/day_3.py
+++ day_3/day_3.py
@@ -2,19 +2,33 @@ import argparse
 import re
 import itertools
 
-def iterate_silver():
-    pass
+def iterate_silver(line):
+    m = 0
+    for ai,a in enumerate([int(c) for c in line]):
+        for bi,b in enumerate([int(c) for c in line]):
+            if ai > bi:
+                x = b * 10 + a
+            elif ai < bi:
+                x = a * 10 + b
+            else:
+                x = 0
+            if x > m:
+                m = x
+                ma,mb = a,b
+
+    return m
 
 def iterate_gold():
     pass
 
-def solve(iterate_f, path="day_2/inputs/input.txt"):
+def solve(iterate_f, path="day_3/inputs/input.txt"):
+    lines = []
     with open(path) as f:
         for line in f.read().split("\n"):
             if line:
-                pass
+                lines.append(iterate_f(line))
 
-    a = 0
+    a = sum(lines)
     print(f"Answer: {a}")
         
 if __name__ == "__main__":
