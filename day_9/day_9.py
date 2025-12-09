38ceb98b460a03a7c20082e1a84d02b6cc2d6866
Matthew Christensen <mchristensen@moosepod.com>
2025-12-09 06:25:23 -0500

    Day 9 part 1

--- day_9/day_9.py
+++ day_9/day_9.py
@@ -5,10 +5,43 @@ import re
 import itertools
 from dataclasses import dataclass
 
-def solve_part_1(path="day_9/inputs/test.txt"):
-    t = time.time()
+type Point = tuple[int,int]
+type PointVector = tuple[Point,Point]
+type Size = tuple[int,int]
 
-    a = 0
+type Rect = tuple[Point,Size]
+
+type Grid = dict[Point,str]
+
+type SizedGrid = tuple[Grid,Size]
+
+# Keys for point/size/rect tuples
+WIDTH = 0
+HEIGHT = 1
+X = 0
+Y = 1
+P = 0
+V = 1
+
+def solve_part_1(path="day_9/inputs/input.txt"):
+    t = time.time()
+    points = []
+    with open(path) as f:
+        for line in f:
+            points.append([int(x) for x in line.strip().split(',')])
+
+    max_area = 0
+    rect = ((0,0),(0,0))
+
+    for p1, p2 in itertools.combinations(points,2):
+        w, h = abs(p1[X]-p2[X])+1,abs(p1[Y]-p2[Y])+1
+        a = w*h
+        if a > max_area:
+            max_area = a
+            rect = (p1,p2)
+
+    print(rect)
+    a = max_area
     print(f"Answer: {a} in {time.time() - t:.2f}s")    
 
 def solve_part_2(path="day_9/inputs/test.txt"):
