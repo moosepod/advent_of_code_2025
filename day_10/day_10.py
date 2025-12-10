e6261628e408c8a3200323af47cc52a1165eafbd
Matthew Christensen <mchristensen@moosepod.com>
2025-12-10 06:39:35 -0500

    Basic machine load

--- day_10/day_10.py
+++ day_10/day_10.py
@@ -5,8 +5,35 @@ import re
 import itertools
 from dataclasses import dataclass
 
+@dataclass
+class Machine:
+    lights: str
+    buttons: list[list[int]]
+
+def extract_buttons(s):
+    buttons = []
+    for b in s.split(" "):
+        lights = []
+        for d in re.findall("(\d+)",b):
+            lights.append(int(d))
+        buttons.append(lights)
+
+    return buttons
+    
+def load_machines(path) -> list[Machine]:
+    machines = []
+    with open(path) as f:
+        for line in f:
+            if m := re.search(r"^\[([#.]+)\] (\(.+\))", line):
+                machines.append(Machine(lights=m.group(1), buttons=extract_buttons(m.group(2))))
+
+    return machines
+    
 def solve_part_1(path="day_10/inputs/test.txt"):
     t = time.time()
+    machines = load_machines(path)
+    for machine in machines:
+        print(machine)
     a = 0
     print(f"Answer: {a} in {time.time() - t:.2f}s")    
 
