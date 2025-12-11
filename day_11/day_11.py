013fccecc31f80d4bcb4b69c5a6079f401a2c341
Matthew Christensen <mchristensen@moosepod.com>
2025-12-11 06:23:25 -0500

    Star 11 day 1

--- day_11/day_11.py
+++ day_11/day_11.py
@@ -5,9 +5,39 @@ import re
 import itertools
 from dataclasses import dataclass
 
-def solve_part_1(path="day_11/inputs/test.txt"):
+def load_graph(path):
+    graph = {}
+    with open(path) as f:
+        for line in f:
+            m = re.match(r"^(.+): (.+)$", line.strip())
+            if m:
+                device = m.group(1)
+                graph[device] = m.group(2).split(" ")
+
+    return graph
+
+def bfs(graph, device, to, seen, path, paths):
+    if device == to:
+        paths.add(",".join(path))
+        return
+
+    #if device in path:
+    #    return
+
+    seen[device] = True
+
+    for device in graph[device]:
+        bfs(graph, device, to, seen, path + [device], paths)
+
+def solve_part_1(path="day_11/inputs/input.txt"):
     t = time.time()
-    a = 0
+    graph = load_graph(path)
+
+    paths = set()
+    bfs(graph, "you","out", {}, ["you"], paths)
+    #for path in paths:
+    #    print(path)
+    a = len(paths)
     print(f"Answer: {a} in {time.time() - t:.2f}s")
 
 def solve_part_2(path="day_11/inputs/test.txt"):
